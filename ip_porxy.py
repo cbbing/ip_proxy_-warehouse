383597aa9b9b20b5c7da6c818f7525553f917c20
cbbing <cbbing@163.com>
2015-12-25 17:21:16 +0800

    get ip

--- ip_porxy.py
+++ ip_porxy.py
@@ -7,13 +7,14 @@ import pandas as pd
 
 from config import engine, mysql_table_ip
 
-class IP_Proxy:
 
-    def __init__(self):
-        pass
 
-    # 获取前100的IP代理地址
-    def get_top_100_ip_proxy(self):
-        sql = 'select * from {0} order by Speed limit 100'.format(mysql_table_ip)
-        df = pd.read_sql_query(sql, engine)
-        return df[['IP', 'Port', 'Type']].get_values()
+# 获取IP代理地址
+def get_ip_proxy(count=100):
+    sql = 'select * from {0} order by Speed limit {1}'.format(mysql_table_ip, count)
+    df = pd.read_sql_query(sql, engine)
+    return df[['IP', 'Port', 'Type']].get_values()
+
+if __name__ == '__main__':
+    values = get_ip_proxy()
+    print values[:10]
