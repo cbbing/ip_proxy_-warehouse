34cafd0d0d73fc34afaa7e736affbc7487792680
cbbing <cbbing@163.com>
2015-12-25 17:40:33 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='IpProxy',
+    version='1.0',
+    description='get available ip proxy',
+    author='Binger',
+    author_email='cbbing@163.com',
+    url='http://www.kekefund.com',
+    py_modules=['ip_porxy', 'config'],
+    )
